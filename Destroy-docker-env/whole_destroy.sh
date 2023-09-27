3a5e2b596bffb6b8ce7ce4cba9168d1d0d4a6281
limonene <limonene@hiroshima-aktk.com>
2023-09-27 10:28:22 +0900

    add clean dev-env shell

---
+++ Destroy-docker-env/whole_destroy.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+echo "Destroying WHOLE docker environment..."
+
+function remove_all {
+  printf 'Are you sure? (y/n): '
+  read input
+  if [ "$input" = 'Y' -o "$input" = 'y' ]; then
+
+    containers=$(docker ps -aq)
+    if [ ! -z "$containers" ]; then
+      docker stop $containers
+      docker rm $containers
+    fi
+
+    docker network prune -f
+
+    dangling_images=$(docker images --filter dangling=true -qa)
+    if [ ! -z "$dangling_images" ]; then
+      docker rmi -f $dangling_images
+    fi
+
+    dangling_volumes=$(docker volume ls --filter dangling=true -q)
+    if [ ! -z "$dangling_volumes" ]; then
+      docker volume rm $dangling_volumes
+    fi
+
+    all_images=$(docker images -qa)
+    if [ ! -z "$all_images" ]; then
+      docker rmi -f $all_images
+    fi
+
+    echo "Done."
+  else
+    echo "Aborted."
+  fi
+}
+
+remove_all
